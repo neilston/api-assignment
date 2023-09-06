836b3be7d9c88408b1bb696062844b8dc1e0043a
Neilston Pinto <neilston@gmail.com>
2023-09-06 16:38:58 +0530

    test 3 changes ess

--- routes/ess.js
+++ routes/ess.js
@@ -17,7 +17,7 @@ Router.get("/",(req,res)=>{
 
 });
 
-//get essay by ids
+//get essay by ids - test 3
 Router.get("/:essid",(req,res)=>{
     mysqlConnection.query("SELECT * from ess WHERE essid =?",[req.params.essid], (err, rows, fields)=>{
         if(!err)
